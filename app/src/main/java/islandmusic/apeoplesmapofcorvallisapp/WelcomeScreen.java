b1f886e2bc357a030489e6d31e90cc78d49c4a27
nwnolte <nwnolte@gmail.com>
2016-10-23 16:53:19 -0700

    Started working on shifting functionality from MapSelector to Welcome Screen, which should be the first activity and class that is called when the app opens up.

---
+++ app/src/main/java/islandmusic/apeoplesmapofcorvallisapp/WelcomeScreen.java
@@ -0,0 +1,21 @@
+package islandmusic.apeoplesmapofcorvallisapp;
+
+/**
+ * Created by HP on 10/23/2016.
+ */
+
+public class WelcomeScreen {
+}
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
