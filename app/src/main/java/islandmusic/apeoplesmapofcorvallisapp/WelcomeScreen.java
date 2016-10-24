48156f1f62abe89da7f4d44ee42fbda0a5b60cac
nwnolte <nwnolte@gmail.com>
2016-10-23 17:48:14 -0700

    expanding welcome screen funct.

--- app/src/main/java/islandmusic/apeoplesmapofcorvallisapp/WelcomeScreen.java
+++ app/src/main/java/islandmusic/apeoplesmapofcorvallisapp/WelcomeScreen.java
@@ -1,8 +1,11 @@
 package islandmusic.apeoplesmapofcorvallisapp;
 
-/**
- * Created by HP on 10/23/2016.
- */
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class WelcomeScreen {
 }
@@ -15,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
+    public void startApp(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
