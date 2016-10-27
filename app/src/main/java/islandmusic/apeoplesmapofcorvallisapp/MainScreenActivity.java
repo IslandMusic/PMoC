6e7bd98bf0d20fcf08d7730faef1889f66c050e6
nwnolte <nwnolte@gmail.com>
2016-10-26 21:51:49 -0700

    Expanded main screen button functions

--- app/src/main/java/islandmusic/apeoplesmapofcorvallisapp/MainScreenActivity.java
+++ app/src/main/java/islandmusic/apeoplesmapofcorvallisapp/MainScreenActivity.java
@@ -2,6 +2,11 @@ package islandmusic.apeoplesmapofcorvallisapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -10,4 +15,35 @@ public class MainScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
     }
+
+    /** Called when the user clicks the Looking button */
+    public void launchLook(View view) {
+        Intent intent = new Intent(this, MapSelector.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    /** Called when the user clicks the Contribute button */
+    public void launchCont(View view) {
+        Intent intent = new Intent(this, Contribute.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    /** Called when the user clicks the About button */
+    public void launchAbout(View view) {
+        Intent intent = new Intent(this, About.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
